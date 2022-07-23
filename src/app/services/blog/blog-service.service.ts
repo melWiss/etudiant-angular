0e669923853e489843f316294a5dfe5927af7f0c
Oussama Maatallah <oussama.linfao.97@gmail.com>
2022-07-23 18:33:05 +0100

    added blog service

---
+++ src/app/services/blog/blog-service.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { baseUrl } from 'src/app/consts';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  all() {
+    return this.http.get(baseUrl + "/blog", {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      }
+    });
+  }
+
+  get(id: number) {
+    return this.http.get(baseUrl + "/blog/" + id, {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      }
+    });
+  }
+
+  commentOnBlog(comment: Comment, id: number) {
+    this.http.post(baseUrl + "/blog/" + id, comment, {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      }
+    });
+  }
+}
